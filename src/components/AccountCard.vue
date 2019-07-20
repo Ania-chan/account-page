b8b442575863a104f86f7bfd536cac4e3588beb7
Ania-chan <ania.jurago@gmail.com>
2019-07-20 21:16:16 +0900

    added basic information to the account page

--- src/components/AccountCard.vue
+++ src/components/AccountCard.vue
@@ -1,5 +1,26 @@
 <template>
-  <h1>Hello World</h1>
+  <div class="card">
+    <div class="card-header">
+      <div class="card-header-title">My Account</div>
+    </div>
+    <figure class="avatar image is-80x80">
+      <img src="https://www.gravatar.com/avatar/HASH?s=80&d=identicon" alt="Profile picture" />
+    </figure>
+    <div class="card-content">
+      <p>
+        Full name:
+        <span>Han Solo</span>
+      </p>
+      <p>
+        Email address:
+        <span>millenium.falcon@gmail.com</span>
+      </p>
+      <p>
+        Password:
+        <span>************</span>
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,4 +30,9 @@ export default {
 </script>
 
 <style scoped>
+.avatar {
+  width: 80px;
+  height: 80px;
+  margin: 20px;
+}
 </style>
